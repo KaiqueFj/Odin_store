b5a0816eea28e1e3567c4798fd7a164f503a7dd3
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-08 22:57:58 -0300

    styling the home page

--- src/components/menu-item/menu_item.component.jsx
+++ src/components/menu-item/menu_item.component.jsx
@@ -4,14 +4,15 @@ import "../../styles/components/menu-item/menu-item.styles.scss";
 
 // informations in the sections
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className= {`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
-      <h1 className="title"> {title}</h1>
+      <h1 className="title"> {title.toUpperCase()}</h1>
       <span className="subtitle">Shop Now</span>
     </div>
   </div>
